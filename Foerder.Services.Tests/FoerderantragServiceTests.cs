cbf288ec894369b07113ea99d6ffe5d55ec0876f
Paul Rohorzka <paul.rohorzka@techtalk.at>
2017-02-08 12:36:17 +0100

    Added first test WithoutBewilligung

---
+++ Foerder.Services.Tests/FoerderantragServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Foerder.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foerder.Services.Tests
+{
+    [TestClass]
+    public class FoerderantragServiceTests
+    {
+        private DateTime AnyStichtag { get; } = DateTime.Now;
+
+        [TestMethod]
+        public void IsAktiv_WithoutBewilligung_ReturnsFalse()
+        {
+            var antrag = new Foerderantrag();
+            var service = new FoerderantragService();
+            
+            // Precondition
+            Assert.IsNull(antrag.Bewilligung);
+
+            // Act
+            var actual = service.IsAktiv(antrag, AnyStichtag);
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
